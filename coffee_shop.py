618f499457dd17198655c0a3327f7224b882b6f5
Brian Gerwig <briangerwig.dev@gmail.com>
2026-08-06 18:11:33 -0400

    Refactor coffee shop into modular functions

--- coffee_shop.py
+++ coffee_shop.py
@@ -1,32 +1,57 @@
-def print_receipt(name, drink, size):
+def get_customer_name():
+    customer_name = input("What is your name? ")
+
+    return customer_name
+
+def get_drink():
+    drink = input("What drink would you like? ").lower()
+    while drink != "coffee" and drink != "tea" and drink != "chai":
+        print("Invalid selection.")
+        drink = input("What drink would you like? (coffee, tea or chai)").lower()
+
+    return drink
+
+def get_size():
+    size = input("What size would you like? ").lower()
+    while size != "small" and size != "medium" and size != "large":
+        print("Invalid size.")
+        size = input("What size would you like? (small, medium or large)").lower()
+
+    return size
+
+def calculate_price(size):
+
+    if size == "large":
+        return 6
+    elif size == "medium":
+        return 5
+    elif size == "small":
+        return 4
+
+def print_receipt(customer_name, drink, size, price):
     print("==========================")
     print("   BRIAN'S COFFEE SHOP")
     print("==========================")
-    print("Customer:", name)
+    print("Customer:", customer_name)
     print("Drink:", drink)
     print("Size:", size)
+    print("Price:", price)
 
-print("Welcome to Brian's Coffee Shop!")
+def main():
+    print("Welcome to Brian's Coffee Shop!")
 
-answer = "yes"
+    answer = "yes"
 
-while answer == "yes":
-    print("Coffee order started.")
+    while answer == "yes":
+        customer_name = get_customer_name()
+        drink = get_drink()
+        size = get_size()
+        price = calculate_price(size)
 
-    name = input("What is your name? ")
-    drink = input("What drink would you like? ")
-    size = input("What size would you like (small, medium, large)? ")
+        print_receipt(customer_name,drink,size,price)
 
-    if size == "large":
-        print("$6.00")
-    elif size == "medium":
-        print("$5.00")
-    elif size == "small":
-        print("$4.00")
-    else:
-         print("Sorry, we don't have that size.")
+        answer = input("Would you like to take another order? (yes/no): ").lower()
 
-    print_receipt(name, drink, size)
-    answer = input("Would you like to take another order? (yes/no): ")
+    print("Thanks for visiting Brian's Coffee Shop!")
 
-print("Thanks for visiting Brian's Coffee Shop!")
+main()
