20504565052614f1261e4409b2e2302f8aaabd64
Brian Gerwig <briangerwig.dev@gmail.com>
2026-08-07 14:25:42 -0400

    Refactor coffee shop into modular functions

--- coffee_shop.py
+++ coffee_shop.py
@@ -1,6 +1,7 @@
 def get_customer_name():
     customer_name = input("What is your name? ")
 
+
     return customer_name
 
 def get_drink():
@@ -11,6 +12,19 @@ def get_drink():
 
     return drink
 
+def get_espresso():
+    shot = input("Would you like to add a shot of espresso? (yes/no) ").lower()
+
+    while shot != "yes" and shot != "no":
+        print("Please answer yes or no.")
+        shot = input("Would you like to add a shot of espresso? (yes/no) ").lower()
+
+    if shot == "yes":
+        return True
+    else:
+        return False
+
+
 def get_size():
     size = input("What size would you like? ").lower()
     while size != "small" and size != "medium" and size != "large":
@@ -19,24 +33,35 @@ def get_size():
 
     return size
 
-def calculate_price(size):
+def calculate_price(size, shot):
 
     if size == "large":
-        return 6
+        price = 6
     elif size == "medium":
-        return 5
+        price = 5
     elif size == "small":
-        return 4
+        price = 4
+    if shot:
+        price = price + 2
 
-def print_receipt(customer_name, drink, size, price):
+    return price
+
+def print_receipt(customer_name,drink,size,shot,price):
     print("==========================")
     print("   BRIAN'S COFFEE SHOP")
     print("==========================")
     print("Customer:", customer_name)
     print("Drink:", drink)
     print("Size:", size)
-    print("Price:", price)
 
+    if shot:
+        shot_display = "Yes"
+    else:
+        shot_display = "No"
+
+    print("Espresso shot:", shot_display)
+    print(f"Price: ${price:.2f}")
+        
 def main():
     print("Welcome to Brian's Coffee Shop!")
 
@@ -46,9 +71,10 @@ def main():
         customer_name = get_customer_name()
         drink = get_drink()
         size = get_size()
-        price = calculate_price(size)
+        shot = get_espresso()
+        price = calculate_price(size,shot)
 
-        print_receipt(customer_name,drink,size,price)
+        print_receipt(customer_name,drink,size,shot,price)
 
         answer = input("Would you like to take another order? (yes/no): ").lower()
 
