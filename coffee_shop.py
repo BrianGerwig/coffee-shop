96bdb872998ebbd6ac0b78c15a53d8871de1789f
Brian Gerwig <briangerwig.dev@gmail.com>
2026-08-11 16:08:54 -0400

    Add order tracking with lists and dictionaries

--- coffee_shop.py
+++ coffee_shop.py
@@ -12,18 +12,31 @@ def get_drink():
 
     return drink
 
+def get_temperature():
+    temperature = input("Would you like your drink hot or iced? ").lower()
+    while temperature != "hot" and temperature != "iced":
+        print("Invalid selection.")
+        temperature = input("Would you like your drink hot or iced? ").lower()
+
+    return temperature
+
+def get_milk():
+    milk = input("What kind of milk would you like? (whole, oat or almond?) ").lower()
+
+    while milk != "whole" and milk != "oat" and milk != "almond":
+        print("Invalid selection.")
+        milk = input("What milk would you like? (whole, oat or almond) ").lower()
+
+    return milk
+
 def get_espresso():
-    shot = input("Would you like to add a shot of espresso? (yes/no) ").lower()
+    shots = int(input("How many shots of espresso would you like? (0-3)"))
 
-    while shot != "yes" and shot != "no":
-        print("Please answer yes or no.")
-        shot = input("Would you like to add a shot of espresso? (yes/no) ").lower()
+    while shots < 0 or shots > 3:
+        print("Invalid selection.")
+        shots = int(input("How many shots of espresso would you like? (0-3)"))
 
-    if shot == "yes":
-        return True
-    else:
-        return False
-
+    return shots
 
 def get_size():
     size = input("What size would you like? ").lower()
@@ -33,7 +46,7 @@ def get_size():
 
     return size
 
-def calculate_price(size, shot):
+def calculate_price(size, shots):
 
     if size == "large":
         price = 6
@@ -41,43 +54,68 @@ def calculate_price(size, shot):
         price = 5
     elif size == "small":
         price = 4
-    if shot:
-        price = price + 2
+
+    price = price + shots * 2
 
     return price
 
-def print_receipt(customer_name,drink,size,shot,price):
+def print_receipt(customer_name,drink,temperature,milk,size,shots,price):
     print("==========================")
     print("   BRIAN'S COFFEE SHOP")
     print("==========================")
     print("Customer:", customer_name)
-    print("Drink:", drink)
+    print("Drink:", temperature, drink)
+    print("Milk:", milk)
     print("Size:", size)
 
-    if shot:
-        shot_display = "Yes"
+    if shots == 1:
+        print("Espresso shot:", shots)
     else:
-        shot_display = "No"
+        print("Espresso shots:", shots)
 
-    print("Espresso shot:", shot_display)
+    print()
+
     print(f"Price: ${price:.2f}")
         
 def main():
     print("Welcome to Brian's Coffee Shop!")
 
     answer = "yes"
+    orders = []
 
     while answer == "yes":
         customer_name = get_customer_name()
         drink = get_drink()
+        temperature = get_temperature()
+        milk = get_milk()
         size = get_size()
-        shot = get_espresso()
-        price = calculate_price(size,shot)
+        shots = get_espresso()
+        price = calculate_price(size,shots)
 
-        print_receipt(customer_name,drink,size,shot,price)
+        order = {
+            "customer": customer_name,
+            "drink": drink,
+            "temperature": temperature,
+            "milk": milk,
+            "size": size,
+            "shots": shots,
+            "price": price,
+        }
+        orders.append(order)
+
+        print_receipt(customer_name,drink,temperature,milk,size,shots,price)
 
         answer = input("Would you like to take another order? (yes/no): ").lower()
 
+    print()
+    print("ORDERS TODAY")
+    print("------------")
+
+    for order in orders:
+        print(f'{order["customer"]} - {order["temperature"]} {order["drink"]} - {order["size"]} - ${order["price"]:.2f}')
+
+    print("Total orders:", len(orders))
+    print()
     print("Thanks for visiting Brian's Coffee Shop!")
 
 main()
